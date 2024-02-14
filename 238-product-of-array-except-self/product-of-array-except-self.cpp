b9bae81dc3c1b94db0cd966a7fca47b0ce6d6669
Sainath Gore <100403114+goresai19@users.noreply.github.com>
2024-02-14 11:03:22 +0530

    Time: 15 ms (78.95%) | Memory: 27.6 MB (7.55%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,22 +4,25 @@ public:
         int zero=0,n=nums.size();
         long long product=1;
         vector<int> ans(n,0);
-        for(int i:nums){
-            (i==0)?(zero++):(product*=i);
-        }
+        vector<int> pre(n,0);
+        vector<int> suf(n,0);
+
         for(int i=0;i<n;i++){
-            if(nums[i]==0 && zero>1)
-            ans[i]=0;
-            if(nums[i]==0 && zero==1){
-                ans[i]=product;
-            }
-            if(nums[i]!=0 && zero==0){
-                ans[i]=product/nums[i];
-            }
-            if(nums[i]!=0 && zero!=0){
-                ans[i]=0;
-            }
+            product*=nums[i];
+            pre[i]=product;
         }
+        product=1;
+
+        for(int i=n-1;i>=0;i--){
+            product*=nums[i];
+            suf[i]=product;
+        }
+       ans[0]=suf[1];
+       ans[n-1]=pre[n-2];
+        for(int i=1;i<n-1;i++){
+              ans[i]=pre[i-1]*suf[i+1];
+        }
+
         return ans;
     }
 };
