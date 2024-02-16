eeec62cfd53cc664613f9f8913f2361dd29f5db1
Sainath Gore <100403114+goresai19@users.noreply.github.com>
2024-02-16 23:25:01 +0530

    Time: 23 ms (74.98%) | Memory: 13.1 MB (12.66%) - LeetSync

---
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findAns(string answerKey,int k,char c){
+        int ans=0,j=0,n=answerKey.size(),maxi=0;
+        for(int i=0;i<n;i++){
+            if(answerKey[i]!=c){
+                k--;
+            }
+            while(k<0 && i>j){
+                if(answerKey[j]!=c){
+                  k++;
+                }
+                j++;
+            }
+            maxi=max(maxi,i-j+1);
+        }
+         return maxi;
+
+    }
+public:
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        return max(findAns(answerKey,k,'T'),findAns(answerKey,k,'F'));
+    }
+};
