a81bfb0d15541f673b4bc6601cf3e1db6228be06
Sainath Gore <100403114+goresai19@users.noreply.github.com>
2024-02-26 10:44:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (46.48%) - LeetSync

--- 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
@@ -2,19 +2,26 @@ class Solution {
 public:
     int minMaxDifference(int num) {
         string s=to_string(num);
-        int n=s.size();
-        int mini=INT_MAX,maxi=INT_MIN;
-        for(int i=0;i<n;i++){
-            string temp_min=s,temp_max=s;
-            for(int j=0;j<n;j++){
-                if(s[j]==s[i]){
-                  temp_min[j]='0';
-                  temp_max[j]='9';
-                }
-            }
-            mini=min(mini,stoi(temp_min));
-            maxi=max(maxi,stoi(temp_max));
+        int n=s.size(),k=0;
+        string temp_min=s,temp_max=s;
+        char mini;
+        while(k<n && s[k]=='9'){
+            k++;
         }
-        return maxi-mini;
+        mini=s[k];
+        cout<<mini<<endl;
+        if(k==n){
+            return num;
+        }
+        for(int i=0;i<n;i++){
+           if(s[i]==mini)
+           temp_min[i]='9';
+           if(s[i]==s[0])
+           temp_max[i]='0';
+        }
+        
+        int max=stoi(temp_min),min=stoi(temp_max);
+
+        return max-min;
     }
 };
