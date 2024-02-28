af16103b4b3d2d72aa697d614913b1b1b67deaa8
Sainath Gore <100403114+goresai19@users.noreply.github.com>
2024-02-28 10:04:04 +0530

    Time: 29 ms (69.08%) | Memory: 24.7 MB (40.92%) - LeetSync

---
+++ 835-linked-list-components/linked-list-components.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int numComponents(ListNode* head, vector<int>& nums) {
+        int ans=0,n=nums.size();
+        sort(nums.begin(),nums.end());
+        int i=0;
+        ListNode* temp=head;
+        set<int> s(nums.begin(),nums.end());
+        while(temp!=nullptr){
+            int k=0;
+             while(temp!=nullptr && s.count(temp->val)){
+                 temp=temp->next;
+                 if(k==0){
+                 ans++;
+                 k=1;
+                 }
+             }  
+             if(temp!=nullptr)
+             temp=temp->next;     
+
+        }
+        return ans;
+    }
+};
