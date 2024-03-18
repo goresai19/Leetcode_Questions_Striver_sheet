ab06bc51746101362daddfeb57ab13a5e34e1eeb
Sainath Gore <100403114+goresai19@users.noreply.github.com>
2024-03-18 12:37:34 +0530

    Time: 30 ms (98.28%) | Memory: 50.9 MB (24.17%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,29 +1,15 @@
 class Solution {
-public:
-   bool canHeJump(vector<int>& nums,int n){
-        int temp=n;
-        while(temp>=0 && nums[temp]!=0){
-            temp--;
-        }
-        if(temp==-1)
-        return true;
-        else{
-            int count=1;
-            for(int i=temp-count;i>=0;i--){
-                if(nums[i]>count){
-                    return canHeJump(nums,i);
-                }
-                count++;
-            }
-        }
-        return false;
-   }
+
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)
-        return true;
-        return canHeJump(nums,n-2);
-
+    int n=nums.size(),maxi=0;
+       for(int i=0;i<n;i++){
+            if(nums[i]==0 && i>=maxi){
+                break;
+            }
+            maxi=max(maxi,i+nums[i]);
+       }
+       cout<<maxi<<endl;
+      return maxi>=n-1;
     }
 };
