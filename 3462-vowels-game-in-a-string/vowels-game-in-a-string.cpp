1c8ac4e8877f96579cdf1875974be1be60d4032d
Sainath Gore <goresai19@gmail.com>
2024-10-17 10:25:42 +0530

    Time: 50 ms (21.65%) | Memory: 17.5 MB (71.11%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int count=0;
+        for(char ch:s){
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                count++;
+            }
+        }
+        if(count==0)
+        return false;
+
+        return true;
+    }
+};
